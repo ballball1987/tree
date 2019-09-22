aba512d6497b6118a1bb75d7caf1d699eb1b4c60
ballball1987 <47935424+ballball1987@users.noreply.github.com>
2019-09-22 21:29:46 +0800

    Update tree0904.py

--- tree/tree0904.py
+++ tree/tree0904.py
@@ -1,3 +1,4 @@
+# 单颗决策数
 # 导入需要的库
 import numpy as np
 import pandas as pd
@@ -8,7 +9,9 @@ from sklearn.tree import DecisionTreeRegressor
 from matplotlib import pyplot as plt
 from sklearn.model_selection import GridSearchCV
 # 导入数据
-pd.set_option('display.max_rows', 100, 'display.max_columns', 50)
+# pd.set_option('display.max_rows', 100, 'display.max_columns', 50)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 data = pd.read_csv('tatandata.csv')
 # print(data.info())
 # print(data['Age'])
@@ -21,6 +24,12 @@ data['Sex'] = (data['Sex'] == 'male').astype('int')
 #  删除空值
 data.drop(['Cabin', 'Ticket', 'Name'], inplace=True, axis=1)
 data = data.dropna()
+
+利用 map函数 处理分类值
+# size_mapping = {'S': 1,
+#                 'C': 2,
+#                 'Q': 3}
+# data['Embarked'] = data['Embarked'].map(size_mapping)
 # 三分类转换数值变量,将数组转换成列表.tolist()
 labels = data['Embarked'].unique().tolist()
 
@@ -94,11 +103,13 @@ score = cross_val_score(clf, x, y, cv=10).mean()
 # {'criterion': 'entropy', 'max_depth': 3, 'min_samples_leaf': 1, 'splitter': 'best'}
 # 0.8215434083601286
 
+# 随机森林
 from sklearn.datasets import load_boston
 from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestRegressor
 # dataset = load_boston()
 # # print(dataset.data.shape)
+# 特征和标签
 # x_full, y_full = dataset.data, dataset.target
 # n_samples = x_full.shape[0]
 # n_features = x_full.shape[1]
@@ -132,6 +143,8 @@ data = load_breast_cancer()
 x = data.data
 y = data.target
 
+
+# n_estimators 调参
 # per_s = []
 # for i in range(35, 49):
 #     rfc = RandomForestClassifier(n_estimators=i, random_state=90)
@@ -163,7 +176,7 @@ y = data.target
 # print(gri.best_score_)
 # {'max_features': 5}
 # 0.9718804920913884
-# maxfeatrue的增加会导致误差变大，前面maxdepth的变大也会增加误差，说明模型已经在上线了，没有参数可以左右了。
+# maxfeatrue的增加会导致误差变大，前面maxdepth的变大也会增加误差，说明模型已经在上限了，没有参数可以左右了。
 # param_grid = {'min_sample_leaf': np.arange(1, 1+10, 1)}
 # rfc = RandomForestClassifier(n_estimators=45, random_state=90)
 # gri = GridSearchCV(rfc, param_grid, cv=10)
@@ -189,7 +202,68 @@ print(gri.best_score_)
 # {'criterion': 'gini'}
 # 0.9718804920913884
 
+#  feature selection
+# 方差过滤
+# from sklearn.feature_selection import VarianceThreshold
+# # print(data.shape, x.shape, y.shape)  # (891, 9) (891, 8) (891,)
+# x_selector_1 = VarianceThreshold().fit_transform(x)
+# print(x_selector_1.shape)  # (891, 8)
+# x_selector_2 = VarianceThreshold(np.median(x.var().values)).fit_transform(x)
+# print(x_selector_2.shape)  # (891, 4)
 
+# 卡方检验  是否独立于标签
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+# chivalue, p_value = chi2(x, y)
+# print(chivalue)
+# print('#'*20)
+# print(p_value)
+# print(p_value < 0.05)
+# # k 的选择
+# k = chivalue.shape[0] - (p_value > 0.05).sum()
+# print(k)
+# print(x.shape)
+# x_selector = SelectKBest(chi2, k=k).fit_transform(x, y)
+# print(x_selector.shape)
+
+# f检验  寻找线性关系
+# from sklearn.feature_selection import f_classif
+# f, p_value_f = f_classif(x, y)
+# k = f.shape[0] - (p_value_f > 0.05).sum()
+# print(k)
+# print(x.shape)
+# x_selector = SelectKBest(f_classif, k=k).fit_transform(x, y)
+# print(x_selector.shape)
+# 6
+# (891, 8)
+# (891, 6)
+
+# 互信息法 返回特征与标签的互信息量，0 式没有，1表示完全相关
+from sklearn.feature_selection import mutual_info_classif as MIC
+# print(MIC(x, y))
+# k = MIC(x, y).shape[0] - (MIC(x, y) < 0).sum()
+# print(k)
+# print(x.shape)
+# x_selector = SelectKBest(MIC, k=k).fit_transform(x, y)
+# print(x_selector.shape)
+# 8
+# (891, 8)
+# (891, 8)
+
+# 包装法  算法和特征选择同时进行， 计算偏慢，不太适合大型数据
+from sklearn.feature_selection import RFE
+# clf = DecisionTreeClassifier()
+# selector = RFE(clf, n_features_to_select=4, step=1).fit(x, y)
+# print(selector.support_.sum())
+# print(x.columns)
+# print(selector.ranking_)
+
+# 嵌入法  使用算法来选择特征  耗时   当prefit式 TRUE时，不能与 cross_val_score, gridsearchcv一起使用
+# from sklearn.feature_selection import SelectFromModel
+# clf = DecisionTreeClassifier()
+# selector = SelectFromModel(clf, threshold=0.05).fit_transform(x, y)
+# print(selector.shape)
+# (891, 5)
 
 
 
